f1928b461f9ea1a7805c3c7f8c1bb1330ec7e40e
douglas-data-analyst <dr970788@gmail.com>
2025-09-21 16:44:13 -0300

    Realização da herança da classe smartphone na classe Iphone e implementei o método abstrato InstalarAplicativo

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    //R:    
+    public class Iphone : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        //R:
+        public Iphone(int numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no iPhone via App Store...");
+            Console.WriteLine($"Aplicativo {nomeApp} instalado com sucesso!");
+        }
     }
 }
